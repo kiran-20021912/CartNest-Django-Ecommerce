8af1ba4d19ac9b3cc6a9c8f33c724c25269de7d3
kiran-20021912 <kirandevang055@gmail.com>
2026-07-02 12:48:26 +0530

    Implemented order placement with Order and OrderItem models

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from .models import Order, OrderItem
+from products.models import Product
 
-# Create your views here.
+
+def place_order(request):
+
+    # Get cart from session
+    cart = request.session.get('cart', {})
+
+    # If cart is empty
+    if not cart:
+        return redirect('cart_detail')
+
+    # Get all products in cart
+    products = Product.objects.filter(id__in=cart.keys())
+
+    # Calculate total price
+    total_price = 0
+
+    for product in products:
+        quantity = cart[str(product.id)]
+        total_price += product.price * quantity
+
+    # Create Order
+    order = Order.objects.create(
+        user=request.user,
+        total_price=total_price
+    )
+
+    # Create OrderItems
+    for product in products:
+        quantity = cart[str(product.id)]
+
+        OrderItem.objects.create(
+            order=order,
+            product=product,
+            quantity=quantity,
+            price=product.price
+        )
+
+    # Clear cart
+    request.session['cart'] = {}
+
+    # Redirect (we'll create this page next)
+    return redirect('home')
